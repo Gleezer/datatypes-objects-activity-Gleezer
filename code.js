f564e553563dc1207ac389f180f98e36d9d784d1
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-06-04 20:28:47 +0000

    deploy: 3658c73ecacdb8a3996989ff0f8cec0042b021ed

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ Objects - Part One
 */
 
 // Do not modify this object here.
-let examplePerson = {
+let person = {
   firstName: "Homer",
   lastName: "Simpson",
   hobbies: ["Television", "Eating Donuts", "Playing with his Kids"],
@@ -15,8 +15,8 @@ let examplePerson = {
     state: "Illinois",
     zipcode: "12345",
   },
-  "favorite color": "yellow",
-};
+  "Favorite Color": "Yellow",
+}
 
 /*
 Using the function below, print the person to the page according 
@@ -24,12 +24,48 @@ to the instructions on my.kenzie
 */
 
 function renderPerson(person) {
-  // Your Code Here
+  let containerDiv = document.createElement('div');
+  containerDiv.className = 'card';
 
+  let nameElement = document.createElement('h2');
+  nameElement.innerText = `${person.firstName} ${person.lastName}`;
+  
+  let hobbiesElement = document.createElement('h4');
+
+  hobbiesElement.innerText = 'Hobbies';
+
+  let hobbiesUlElement = document.createElement('ul');
+
+  for (let index = 0; index < person.hobbies.length; index++) {
+    let hobbiesIndex = person.hobbies[index];
+    let listItem = document.createElement('li');
+    listItem.innerText = hobbiesIndex;
+    hobbiesUlElement.append(listItem);
+
+  }
+
+  let addressElement = document.createElement('h4');
+  addressElement.innerText = 'Shipping Address';
+  let addressText = document.createElement('p');
+
+  addressText.innerText = `${person.firstName} ${person.lastName}
+  ${person.address.number} ${person.address.street}
+   ${person.address.city}, ${person.address.state} ${person.address.zipcode}`;
+
+  let favColorElement = document.createElement('h4');
+  favColorElement.innerText = ('Favorite Color');
+
+  let favColorText = document.createElement('p');
+  favColorText.innerText = `${person["Favorite Color"]}`;
+
+  
+
+  document.body.append(containerDiv);
+  containerDiv.append(nameElement, hobbiesElement, hobbiesUlElement, addressElement, addressText, favColorElement, favColorText);
 
 }
 
-renderPerson(examplePerson);
+renderPerson(person);
 
 /*
 ---------------------------------------------------------------------------
@@ -45,9 +81,20 @@ render your person to the page.
 */
 
 // Your Code Here
-
-
-
+let newPerson = {
+  firstName: "Cole",
+  lastName: "Craft",
+  hobbies: ["Xbox", "Comics", "D&D"],
+  address: {
+    number: 1213,
+    street: "Carriage Parc Drive",
+    city: "Chattanooga",
+    state: "Tennessee",
+    zipcode: "37421",
+  },
+  "Favorite Color": "Dark Purple",
+}
+renderPerson(newPerson);
 
 /*
 ---------------------------------------------------------------------------
@@ -59,13 +106,21 @@ Follow the instruction on my.kenzie
 
 function changePersonToBart(person) {
   // Your Code Here
+  person.firstName = 'Bart';
+  person["Favorite Color"] = "Orange";
+  person.hobbies.pop();
+  person.hobbies.push('Skateboarding');
 
-
+  return person;
 }
 
 // Then, call the function with the argument examplePerson
 // Assign the result to a new variable.  Then call renderPerson with the 
 // new variable
+
+let changeToBart = changePersonToBart(person);
+
+renderPerson(changeToBart);
 
 /*
 ---------------------------------------------------------------------------
